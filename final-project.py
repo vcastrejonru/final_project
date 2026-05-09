b1af5fea1ef9e1f08f8926890943912472543bc5
Valentina Castrejon <vtinacastrejon@gmail.com>
2026-05-09 18:01:04 -0500

    particles are finally working!!!!1

--- final-project.py
+++ final-project.py
@@ -8,21 +8,34 @@ import pygame
     # + math for light sway of leaves falling
     # make the sprites choose random color based on the image
 
+image = pygame.image.load('illustartion_RMV.jpg')
+width, height = image.get_size()
+particle_img = pygame.image.load('leaf.png')
+particle_img = pygame.transform.scale(particle_img, (15, 15))
 
+class Particle:
+    def __init__(self):
+        self.x = random.randint(0, width)
+        self.y = random.randint(0, height)
+        self.speed = random.randint(5, 15)
 
-      
+    def fall(self):
+        self.y += self.speed #this will keep the  particle movign downard, its updating its position w the speed
+        if self.y > height: #this is too see if the particle has fallen out of the sceen
+            self.y = random.randint(-height, 0) #this resets the particle above the screen (avoids syncornized falling, thanks sam)
+    
+    def draw(self, screen):
+        screen.blit(particle_img, (self.x, self.y))
+
+particles = []
+for i in range(50):
+    particles.append(Particle())
 
 
 def main():
     pygame.init()
     pygame.display.set_caption('Pixel Art Screen')
-    #clock = pygame.time.Clock()
-    #dt = 0
-    #particle_img = pygame.image.load('leaf.png')
-   # particle_img = pygame.transform.scale(particle_img, (15, 15))
-   # particles = []
-    image = pygame.image.load('illustartion_RMV.jpg')
-    width, height = image.get_size()
+    clock = pygame.time.Clock()
     resolution = (width, height)
     small_img = pygame.transform.scale(image, (width//8, height//8))
     pixelated_img = pygame.transform.scale(small_img, resolution)
@@ -45,8 +58,11 @@ def main():
             screen.blit(pixelated_img, (0, 0))
         else:
             screen.blit(image, (0, 0))
+        
+        for particle in particles:
+            particle.fall()
+            particle.draw(screen)
         pygame.display.flip()
-        #dt = clock.tick(12)
     pygame.quit()
 
 if __name__ == "__main__":
