8a203821eed415ab0389c4dfc55d58c931851456
Valentina Castrejon <vtinacastrejon@gmail.com>
2026-05-08 21:07:56 -0500

    starting over, i dont know what im doing. new leaf images for particles

--- final-project.py
+++ final-project.py
@@ -8,45 +8,19 @@ import pygame
     # + math for light sway of leaves falling
     # make the sprites choose random color based on the image
 
-class Particle():
 
-    def __init__(self, pos=(1, 0), size=(10), life=1000):
-        self.pos = pos
-        self.size = size
-        self.color = pygame.Color(self.random_color())
-        self.age = 0
-        self.life = life
-        self.dead = False
-        self.alpha = 255
-        self.surface = self.update_surface()
 
-    def random_color(self):
-
-        x = random.randint(0, self.image.get_width())
-        y = random.randint(0, self.image.get_height()) 
-
-    def update(self, dt):
-        self.age += dt
-        if self.age > self.life:
-             self.dead = True
-        self.alpha = 255 * (1 - (self.age / self.life))
-
-    def update_surface(self):
-        
-        #sprites?
-    def draw(self, surface):
-        if self.dead:
-            return
-        self.surface.set_alpha(self.alpha)
-        surface.blit(self.surface, self.pos)
-
+      
 
 
 def main():
     pygame.init()
     pygame.display.set_caption('Pixel Art Screen')
-    clock = pygame.time.Clock()
-    dt = 0
+    #clock = pygame.time.Clock()
+    #dt = 0
+    #particle_img = pygame.image.load('leaf.png')
+   # particle_img = pygame.transform.scale(particle_img, (15, 15))
+   # particles = []
     image = pygame.image.load('illustartion_RMV.jpg')
     width, height = image.get_size()
     resolution = (width, height)
@@ -72,7 +46,7 @@ def main():
         else:
             screen.blit(image, (0, 0))
         pygame.display.flip()
-        dt = clock.tick(12)
+        #dt = clock.tick(12)
     pygame.quit()
 
 if __name__ == "__main__":
